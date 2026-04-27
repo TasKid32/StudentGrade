91b2d47edb13c8c430e1f692bb8485ff7d24cf28
TasKid32 <203917777+TasKid32@users.noreply.github.com>
2026-04-28 02:43:57 +0300

    Merge branch 'Re-Implement_Junit5' into master.

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -11,6 +11,7 @@ public class StudentGradeTest {
 
     @Test
     public void testGetGrade95() {
+        System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
@@ -19,6 +20,7 @@ public class StudentGradeTest {
 
     @Test
     public void testGetGrade85() {
+        System.out.println("getGrade 85");
         int mark = 85;
         String expResult = "B";
         String result = StudentGrade.getGrade(mark);
@@ -27,6 +29,7 @@ public class StudentGradeTest {
 
     @Test
     public void testGetGrade75() {
+        System.out.println("getGrade 75");
         int mark = 75;
         String expResult = "F"; 
         String result = StudentGrade.getGrade(mark);
